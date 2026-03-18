425680b68f65fcadfee957dabc6d41b296f8ca35
CoderLakshyaYadav <yadavl@s.dcsdk12.org>
2026-03-18 10:22:12 -0600

    Completed BasicCalculator.py

---
+++ BasicCalculator.py
@@ -0,0 +1,25 @@
+def add(x, y):
+    answer = x + y
+    return answer
+
+def subtract(x, y):
+    answer = x - y
+    return answer
+
+def multiply(x, y):
+    answer = x * y
+    return answer
+
+def divide(x, y):
+    answer = x / y
+    return answer
+
+def exp(x, y):
+    answer = x**y
+    return answer
+
+print(add(5, 2))
+print(subtract(1, 2))
+print(multiply(7, 2))
+print(divide(81, 9))
+print(exp(2, 6))
